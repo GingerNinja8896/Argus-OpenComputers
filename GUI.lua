2bad89607b4479008fc74b0338bfb9a84b2edc0d
GingerNinja8896 <jonahkaplan@hotmail.com>
2021-08-09 22:21:00 +0100

    Added Bars and Buttons

--- GUI.lua
+++ GUI.lua
@@ -13,6 +13,8 @@ GUI.red = 0xFF0000
 pages = {}
 currentPage = nil
 
+e
+
 function GUI.Label(page,x,y,text,colour,updateFunc)
     local self = {}
     self.draw = DrawLabel
@@ -21,7 +23,7 @@ function GUI.Label(page,x,y,text,colour,updateFunc)
     self.text = text
     self.colour = colour
     self.updateFunc = updateFunc
-    table.insert(page.labels.widgets,self)
+    table.insert(page.widgets,self)
     return self
 end
 
@@ -33,7 +35,21 @@ function GUI.Bar(page,x,y,width,height,getValue)
     self.width = width
     self.height = height
     self.getValue = getValue
-    table.insert(page.labels.widgets,self)
+    table.insert(page.widgets,self)
+    return self
+end
+
+function GUI.Button(page,x,y,width,height,text,centered,OnPress)
+    local self = {}
+    self.draw = DrawButton
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.text = text
+    self.OnPress = OnPress
+    self.centered = centered
+    table.insert(page.widgets,self)
     return self
 end
 
@@ -46,31 +62,52 @@ function GUI.Page(set)
     self.name = {}
     self.widgets = {}
     table.insert(pages,self)
-    if set then SetPage(page) end
+    if set then GUI.SetPage(self) end
+    print(self)
     return self
 end
 function GUI.ClearScreen()
-    gpu.setBackground(black)
+    gpu.setBackground(GUI.black)
     gpu.fill(1,1,w,h," ")
 end
 
-function DrawLabel()
-    gpu.setBackground(white)
+function DrawLabel(label)
+    gpu.setBackground(GUI.white)
     gpu.setForeground(label.colour)
     gpu.set(label.x,label.y,label.text)
 
 end
 
 function DrawBar(bar)
-    gpu.setBackground(red)
+    gpu.setBackground(GUI.red)
     local value = bar.getValue()
     gpu.fill(bar.x,bar.y,bar.width*value,bar.height," ")
-    gpu.setBackground(white)
+    gpu.setBackground(GUI.white)
     gpu.fill(bar.x + bar.width*value,bar.y,(1-value)*bar.width,bar.height," ")
 end
 
-function GUI.Draw()
+function DrawButton(button)
+    gpu.setBackground(GUI.white)
+    gpu.fill(button.x,button.y,button.width,button.height," ")
+    if button.centered then local x = (button.width - #button.text)*0.5 else local x = button.x+1 end
+    gpu.set(x,button.y/2+0.5,button.text)
+end
+
+function OnClick(address,x,y,button)
+    if button == nil then return end
+
     for _,widget in ipairs(widgets) do
+        if widget.OnPress ~= nil then
+            if x>=widget.x and y>= widget.y and x<=widget.x+widget.width and y <= widget.y + widget.height then
+                widget.OnPress()
+            end
+        end
+    end
+end
+
+function GUI.Draw()
+    UI.ClearScreen()
+    for _,widget in ipairs(currentPage.widgets) do
         widget.draw(widget)
     end
 end
--- PowerDistributor.lua
+++ PowerDistributor.lua
@@ -2,6 +2,7 @@ local gui = require("GUI")
 
 page1 = gui.Page(true)
 
-gui.ClearScreen()
 gui.Label(page1,1,1,"hey",gui.black)
+gui.Bar(page1,10,10,10,4,function() return 0.6  end)
+test = gui.Button(page1,20,20,8,4,"click",true,function()  test.text = "good" end)
 gui.Draw()
